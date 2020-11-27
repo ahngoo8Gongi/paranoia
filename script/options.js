f19de6c2421c8fdce730bbcc6e750858a2b51773
Your Name <you@example.com>
2020-11-27 17:41:32 +0100

    linted HTML

---
+++ script/options.js
@@ -0,0 +1,8 @@
+/**
+ * @file Paranoia extension for Mozilla Firefox
+ * @author Holger Smolinski
+ * @copyright (C), 2020 Holger Smolinski
+ * @license MPL-2.0 This Source Code Form is subject to the terms of the Mozilla
+ *          Public License, v. 2.0. If a copy of the MPL was not distributed
+ *          with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
